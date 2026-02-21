df10f6c6a5ec9a53c9b80a0cc8a10512d7004d15
Leo741776 <leo.aghazaryan.483@my.csun.edu>
2026-02-20 22:55:23 -0800

    Added 'Brokerage' object to 'User.cs'

--- User.cs
+++ User.cs
@@ -7,11 +7,13 @@ public class User
     private string name;
     private Checking checkingAccount;
     private Savings savingsAccount;
+    private Brokerage brokerageAccount;
 
-    public User(string name, Checking checkingAccount, Savings savingsAccount)
+    public User(string name, Checking checkingAccount, Savings savingsAccount, Brokerage brokerageAccount)
     {
         this.name = name;
         this.checkingAccount = checkingAccount;
         this.savingsAccount = savingsAccount;
+        this.brokerageAccount = brokerageAccount;
     }
 }
