5fee17fb04985baa5b32606db219db8af204143c
Leo <leo.aghazaryan.483@my.csun.edu>
2026-03-06 20:29:24 -0800

    Add StockApiService.cs
    A stock api is used to retrieve the latest stock prices and parse them into
    usable data for our program. The API is called in StockApiService.cs and
    the data is managed within the Brokerage.cs class.

---
+++ backend/User.cs
@@ -0,0 +1,19 @@
+﻿// User.cs
+// Stores user's info
+// Contains Wallet, Savings, and Brokerage class instances
+
+public class User
+{
+    private string name;
+    private Wallet walletAccount;
+    private Savings savingsAccount;
+    private Brokerage brokerageAccount;
+
+    public User(string name, Wallet walletAccount, Savings savingsAccount, Brokerage brokerageAccount)
+    {
+        this.name = name;
+        this.walletAccount = walletAccount;
+        this.savingsAccount = savingsAccount;
+        this.brokerageAccount = brokerageAccount;
+    }
+}
